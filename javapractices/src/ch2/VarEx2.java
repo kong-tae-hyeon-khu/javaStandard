6dd676a9dd75f35bc427a2b33a68d3d948d3420a
kongtaehyeon-khu <kong3047@khu.ac.kr>
2023-01-02 19:18:35 +0900

    Practice Chapter 2 : Variable at 2023 02 January

---
+++ javapractices/src/ch2/VarEx2.java
@@ -0,0 +1,16 @@
+package ch2;
+
+public class VarEx2 {
+	public static void main(String[] args) {
+		int x = 10, y = 20;
+		int temp = 0;
+		
+		System.out.println("x : " +  x + " y : " + y);
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("x : " + x + " y : " + y);
+	}
+}
