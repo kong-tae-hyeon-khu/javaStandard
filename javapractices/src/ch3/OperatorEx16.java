803aa93f249c1c23d24013d42a6091159031a9fe
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-01-09 22:48:04 +0900

    Practice Operator at January 9 2023

---
+++ javapractices/src/ch3/OperatorEx16.java
@@ -0,0 +1,9 @@
+package ch3;
+
+public class OperatorEx16 {
+	public static void main(String[] args) {
+		float pi = 3.141592f;
+		float shortPi = (int)(pi * 1000) / 1000f;
+		System.out.println(shortPi);
+	}
+}
