c357910e18446a1cb24355287796ffefef6271c9
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-01-09 22:58:03 +0900

    remainder operation

---
+++ javapractices/src/ch3/OperatorEx20.java
@@ -0,0 +1,9 @@
+package ch3;
+
+public class OperatorEx20 {
+	public static void main(String args[]) {
+		System.out.println(-10%8);  // -2
+		System.out.println(10%-8);  // 2
+		System.out.println(-10%-8); // -2
+	}
+}
