0661ffcbb08866dbb2efdf57b65ea842f12c573d
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-02-12 11:36:46 +0900

    Practice Polymolphysm (다형성)

---
+++ javapractices/ch7/BindingTest2.java
@@ -0,0 +1,24 @@
+class Parent {
+    int x = 100;
+    void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child extends Parent {
+
+}
+
+
+public class BindingTest2 {
+    public static void main(String args[]) {
+        Parent p = new Child();
+        Child c = new Child();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+}
