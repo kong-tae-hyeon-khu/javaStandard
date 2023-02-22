35745aee8c90c46a1bc6df801d335370ac0fd3f8
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-02-22 15:51:43 +0900

    String Class

---
+++ javapractices/ch9/StringEx3.java
@@ -0,0 +1,9 @@
+public class StringEx3 {
+    public static void main(String args[]) {
+        char[] chArr = new char[0];
+        String s = new String(chArr);
+
+        System.out.println("cArr.length = " + cArr.length);
+        System.out.println("@@@" + s + "@@@");
+    }
+}
